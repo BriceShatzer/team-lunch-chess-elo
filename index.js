7c609e3d0b89e3b3b8392368cf6c44a9afbdb329
Brice Shatzer <bshatzer@kinja.com>
2018-11-27 13:35:02 -0600

    added a game

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ let games = [
 	createGame(Brice_Shatzer, Bryce_Dorn, 0),
 	createGame(Brice_Shatzer, Danny_Cheng, 1), // white = Brice?
 	createGame(Danny_Cheng, Brice_Shatzer, 1, new Date('11/26/2018')), // white = Danny
-	createGame(Jamie_Levinson, Mike_Parent, 0)
+	createGame(Jamie_Levinson, Mike_Parent, 0),
+        createGame(Joe_Duran, Brice_Shatzer, 0, new Date('11/27/2018'))
 ]
 
 
